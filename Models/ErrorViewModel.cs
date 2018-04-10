e4f4f1814252d6968d3b50ca4ba10c847e0f64be
Thomas Asadurian <Tasadurian@gmail.com>
2018-04-09 19:21:02 -0700

    initial boilerplate

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace my_recipe_book.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
